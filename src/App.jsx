32610cb2c4a2a360adc1f26afbfdf654141bf8eb
Sheisa Firstavrilla <astynsheisa@gmail.com>
2024-09-17 15:02:08 +0700

    menambahkan halaman contact dan contact form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // import pages
 import Home from './pages/Home'
 import Search from './pages/Search'
+import Contact from './pages/Contact'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import ContactForm from '../components/ContactForm'
+
+export default function Contact() {
+    return (
+        <>
+            <Navbar />
+            <ContactForm />
+            <Footer />
+        </>
+    )
+}
