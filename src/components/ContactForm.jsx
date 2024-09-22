a5d49440f9fd6bbe9fa1bd8a7ea46319c77f18d6
Sheisa Firstavrilla <astynsheisa@gmail.com>
2024-09-22 20:35:35 +0700

    menambahkan teks contact us

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,67 +2,69 @@ import React from 'react';
 
 export default function ContactForm() {
     return (
-        <form className="max-w-sm mx-auto border border-gray-200 rounded-lg shadow p-5">
-            <div className="mb-5">
-                <label
-                    htmlFor="name"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                    Name
-                </label>
-                <input
-                    type="text"
-                    id="name"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Your name"
-                    required=""
-                />
-            </div>
-            <div className="mb-5">
-                <label
-                    htmlFor="email"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                    Your email
-                </label>
-                <input
-                    type="email"
-                    id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@flowbite.com"
-                    required=""
-                />
-            </div>
-
-            <form class="max-w-sm mx-auto">
-                <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
-                <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
-            </form>
-
-            <div className="flex items-start mb-5 mt-2">
-                <div className="flex items-center h-5">
+        <>
+            <h1 className='text-black text-center font-bold text-3xl p-10'>Contact Us</h1>
+            <form className="max-w-sm mx-auto border border-gray-200 rounded-lg shadow p-5">
+                <div className="mb-5">
+                    <label
+                        htmlFor="name"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                        Name
+                    </label>
                     <input
-                        id="remember"
-                        type="checkbox"
-                        defaultValue=""
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                        type="text"
+                        id="name"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Your name"
                         required=""
                     />
                 </div>
-                <label
-                    htmlFor="remember"
-                    className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
-                    Remember me
-                </label>
-            </div>
-            <button
-                type="submit"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-                Submit
-            </button>
-        </form>
+                <div className="mb-5">
+                    <label
+                        htmlFor="email"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                        Your email
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="name@flowbite.com"
+                        required=""
+                    />
+                </div>
 
-    );
+                <form class="max-w-sm mx-auto">
+                    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+                    <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+                </form>
+
+                <div className="flex items-start mb-5 mt-2">
+                    <div className="flex items-center h-5">
+                        <input
+                            id="remember"
+                            type="checkbox"
+                            defaultValue=""
+                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                            required=""
+                        />
+                    </div>
+                    <label
+                        htmlFor="remember"
+                        className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                    >
+                        Remember me
+                    </label>
+                </div>
+                <button
+                    type="submit"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                    Submit
+                </button>
+            </form>
+        </>
+    )
 }
