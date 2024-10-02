954fea0bb2b017f7d4028be518001032d9c08d8a
Sheisa Firstavrilla <astynsheisa@gmail.com>
2024-10-02 22:09:36 +0700

    merapikan halaman list recipe (tp belum sesuai)

--- src/components/ui/CardList.jsx
+++ src/components/ui/CardList.jsx
@@ -2,8 +2,33 @@ import React from 'react'
 
 export default function CardList() {
   return (
-    <div>
-      
+    <div className='px-20'>
+      <div className='h-60'>
+        <div className='relative overflow-x-auto shadow-md sm:rounded-lg mt-10'>
+          <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-pink-400'>
+            <thead className='text-xs text-gray-700 uppercase bg-pink-50 dark:bg-pink-700 dark:text-pink-400'>
+              <tr>
+                <th scope="col" className="px-16 py-3">
+                  <span className="sr-only">Image</span>
+                </th>
+                <th scope="col" className="px-6 py-3 text-center">
+                  Recipe name
+                </th>
+                <th scope="col" className="px-6 py-3 text-center">
+                  Description
+                </th>
+                <th scope="col" className="px-6 py-3 text-center">
+                  Edit
+                </th>
+                <th scope="col" className="px-6 py-3 text-center">
+                  Delete
+                </th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/blocks/Navbar'
-import TableList from '../components/TableList'
+import CardList from '../components/ui/CardList'
 import Footer from '../components/blocks/Footer'
+import TableList from '../components/TableList'
 
 export default function List() {
     return (
         <>
             <Navbar />
+            {/* <CardList /> */}
             <TableList />
             <Footer />
         </>
