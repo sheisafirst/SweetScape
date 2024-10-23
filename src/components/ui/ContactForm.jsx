7d569a25102ef6f39d32c42d7bbda75a3e9fff3a
Sheisa Firstavrilla <astynsheisa@gmail.com>
2024-10-23 22:36:13 +0700

    merapikan dan menghias halaman contact

--- src/components/ui/ContactForm.jsx
+++ src/components/ui/ContactForm.jsx
@@ -3,80 +3,93 @@ import React from 'react';
 export default function ContactForm() {
     return (
         <>
-            <h1 className='text-black text-center font-bold text-3xl p-10'>Contact Us</h1>
-            <form className="max-w-sm mx-auto border border-gray-200 rounded-lg shadow p-5">
-                <div className="mb-5">
-                    <label
-                        htmlFor="name"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                        Name
-                    </label>
-                    <input
-                        type="text"
-                        id="name"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Your name"
-                        required=""
-                    />
-                </div>
-                <div className="mb-5">
-                    <label
-                        htmlFor="email"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                        Your email
-                    </label>
-                    <input
-                        type="email"
-                        id="email"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="name@flowbite.com"
-                        required=""
-                    />
-                </div>
+            <div className='px-8 pt-10'>
+                <section className='relative'>
+                    <img src="/contact1.jpg" alt="baking" className='absolute w-full h-full object-cover -z-10 rounded-lg' style={{ opacity: 0.5 }} />
+                    <div className='grid grid-cols-2 gap-10 p-5'>
+                        <div>
 
-                <form className="max-w-sm mx-auto">
-                    <label
-                        htmlFor="message"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                        Your message
-                    </label>
-                    <textarea
-                        id="message"
-                        rows={4}
-                        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Leave a comment..."
-                        defaultValue={""}
-                    />
-                </form>
+                        </div>
+                        <div>
+                            <form className="max-w-sm mx-auto bg-pink-50 border border-pink-200 rounded-xl p-6 shadow-xl shadow-pink-300/50 hover:shadow-pink-400/40 transition-all duration-300 ease-in-out"
+                            >
+                                <h1 className='text-black text-center font-bold text-3xl pb-5'>Contact Us</h1>
+                                <div className="mb-5">
+                                    <label
+                                        htmlFor="name"
+                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    >
+                                        Name
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="name"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="Your name"
+                                        required=""
+                                    />
+                                </div>
+                                <div className="mb-5">
+                                    <label
+                                        htmlFor="email"
+                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    >
+                                        Your email
+                                    </label>
+                                    <input
+                                        type="email"
+                                        id="email"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="name@flowbite.com"
+                                        required=""
+                                    />
+                                </div>
+
+                                <form className="max-w-sm mx-auto">
+                                    <label
+                                        htmlFor="message"
+                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    >
+                                        Your message
+                                    </label>
+                                    <textarea
+                                        id="message"
+                                        rows={4}
+                                        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="Leave a comment..."
+                                        defaultValue={""}
+                                    />
+                                </form>
 
 
-                <div className="flex items-start mb-5 mt-2">
-                    <div className="flex items-center h-5">
-                        <input
-                            id="remember"
-                            type="checkbox"
-                            defaultValue=""
-                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                            required=""
-                        />
+                                <div className="flex items-start mb-5 mt-2">
+                                    <div className="flex items-center h-5">
+                                        <input
+                                            id="remember"
+                                            type="checkbox"
+                                            defaultValue=""
+                                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                                            required=""
+                                        />
+                                    </div>
+                                    <label
+                                        htmlFor="remember"
+                                        className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                    >
+                                        Remember me
+                                    </label>
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                >
+                                    Submit
+                                </button>
+                            </form>
+                        </div>
                     </div>
-                    <label
-                        htmlFor="remember"
-                        className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                    >
-                        Remember me
-                    </label>
-                </div>
-                <button
-                    type="submit"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                    Submit
-                </button>
-            </form>
+                </section>
+            </div>
         </>
     )
 }
