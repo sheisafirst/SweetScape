d6c84c19a3123baeeae94672f2037e3690bef573
Sheisa Firstavrilla <astynsheisa@gmail.com>
2024-10-26 21:07:09 +0700

    update halaman login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Navbar from '../components/blocks/Navbar'
-import Footer from '../components/blocks/Footer'
+import NavbarAdmin from '../components/blocks/NavbarAdmin'
+import FooterAdmin from '../components/blocks/FooterAdmin'
 import LoginForm from '../components/blocks/LoginForm'
 
 export default function Login() {
     return (
         <>
-            <Navbar />
+            <NavbarAdmin />
             <LoginForm />
-            <Footer />
+            <FooterAdmin />
         </>
     )
 }
